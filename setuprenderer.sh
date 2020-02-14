ca2e3ef368c8dbe97b148ebaff41360daea157dc
DaCoolX <2771553+DaCoolX@users.noreply.github.com>
2020-02-14 17:37:33 +0100

    Drop sudo

--- setuprenderer.sh
+++ setuprenderer.sh
@@ -6,9 +6,10 @@ useradd --no-create-home \
 	--uid $user_UID \
 	--gid $user_GID \
 	--inactive 0 \
-	--shell /usr/sbin/nologin \
 	sheepit
 chown -c sheepit:sheepit ./runrenderer.sh
 chmod -c ug+x ./runrenderer.sh
 chown -Rc sheepit:sheepit /sheep-cache
-sudo -E -u sheepit "/sheep/runrenderer.sh"
+su --command "/sheep/runrenderer.sh" \
+	--preserve-environment \
+	sheepit
