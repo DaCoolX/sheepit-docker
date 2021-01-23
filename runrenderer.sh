f41dd2ff0978c1c81f87b532a518dd7802ddfa47
DaCoolX <2771553+DaCoolX@users.noreply.github.com>
2021-01-23 17:53:27 +0100

    General improvments to runrenderer.sh
    Adopt changes taken or inspired by https://github.com/AGSPhoenix/sheepit-docker/pull/14

--- runrenderer.sh
+++ runrenderer.sh
@@ -3,16 +3,16 @@
 echo "Checking for client updates..."
 cd /sheep-cache
 
-latestVersion=`curl --silent --head https://www.sheepit-renderfarm.com/media/applet/client-latest.php | \
-    grep -Po '(?i)content-disposition:.*filename="?(?-i)\Ksheepit-client-[\d\.]+\d'`
+latestVersion=$(curl --silent --head https://www.sheepit-renderfarm.com/media/applet/client-latest.php | \
+    grep -Po '(?i)content-disposition:.*filename="?(?-i)\Ksheepit-client-[\d\.]+\d')
     
-if [ -z $latestVersion ]; then
+if [ -z "$latestVersion" ]; then
     #Empty latestVersion is a good indicator of a critical failure
-    echo !!! Failed parsing version information! Aborting !!!
-    echo Possible causes and troubleshooting steps:
-    echo 1. Check for internet connectivity \(Routes, DNS, Proxy\)
-    echo 2. Check the status of SheepIt via the SheepIt website: https://www.sheepit-renderfarm.com/
-    echo 3. Open an issue on Github if problems persists after 1. and 2.
+    echo "!!! Failed parsing version information! Aborting !!!"
+    echo "Possible causes and troubleshooting steps:"
+    echo "1. Check for internet connectivity \(Routes, DNS, Proxy\)"
+    echo "2. Check the status of SheepIt via the SheepIt website: https://www.sheepit-renderfarm.com/"
+    echo "3. Open an issue on Github if problems persists after 1. and 2."
     exit 1
 elif [ ! -e $latestVersion.jar ]; then
 	echo "Updating client..."
@@ -28,8 +28,8 @@ echo "Starting client:"
 
 java -jar /sheep-cache/$latestVersion.jar \
 	-ui text \
-	-login $user_name \
-	-password $user_password \
+	-login "$user_name" \
+	-password "$user_password" \
 	-cores $(nproc) \
 	-cache-dir /sheep-cache \
 	$extra_opt
