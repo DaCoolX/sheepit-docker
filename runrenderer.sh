846cf7047a19ceaebbd464faa57e1d84bcf0708b
DaCoolX <2771553+DaCoolX@users.noreply.github.com>
2021-05-13 08:52:19 +0200

    Enable beta client

--- runrenderer.sh
+++ runrenderer.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-echo "Checking for client updates..."
+echo "Checking for beta client..."
 cd /sheep-cache
 
-latestVersion=$(curl --silent --head https://www.sheepit-renderfarm.com/media/applet/client-latest.php | \
-    grep -Po '(?i)content-disposition:.*filename="?(?-i)\Ksheepit-client-[\d\.]+\d')
-    
-if [ -z "$latestVersion" ]; then
-    #Empty latestVersion is a good indicator of a critical failure
-    echo "!!! Failed parsing version information! Aborting !!!"
-    echo "Possible causes and troubleshooting steps:"
-    echo "1. Check for internet connectivity \(Routes, DNS, Proxy\)"
-    echo "2. Check the status of SheepIt via the SheepIt website: https://www.sheepit-renderfarm.com/"
-    echo "3. Open an issue on Github if problems persists after 1. and 2."
-    exit 1
-elif [ ! -e $latestVersion.jar ]; then
-	echo "Updating client..."
-	rm -f sheepit-client*.jar
-	#Download new client.
-	curl https://www.sheepit-renderfarm.com/media/applet/client-latest.php -o $latestVersion.jar
-	echo "Update finished"
-else
-	echo "No updates found"
+latestVersion=sheepit-client-6.21131.0
+
+if [ ! -e $latestVersion.jar ]; then
+	curl https://www.sheepit-renderfarm.com/media/applet/$latestVersion.jar -o $latestVersion.jar
+	if [ $? -ne 0 ]; then
+    		#Unclean exit is a good indicator of a critical failure
+    		echo "!!! Failed client download! Aborting !!!"
+    		echo "Possible causes and troubleshooting steps:"
+    		echo "1. Check for internet connectivity \(Routes, DNS, Proxy\)"
+    		echo "2. Check the status of SheepIt on the Blender Discord"
+    		echo "3. Open an issue on Github if problems persists after 1. and 2."
+    		exit 1
+	fi
 fi
 
-echo "Starting client:"
+echo "Starting beta client:"
 
 java -jar /sheep-cache/$latestVersion.jar \
 	-ui text \
